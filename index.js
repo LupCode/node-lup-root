e8c8c46c84571733f4894fecabb791e6fa7a611f
Luca Vogels <luporlp.yt@gmail.com>
2022-05-03 23:25:36 +0200

    Improved support for TypeScript

--- index.js
+++ index.js
@@ -1,31 +1,34 @@
-var _MAIN = __filename;
-var _ROOT = __dirname;
-var limit = Error.stackTraceLimit;
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ROOT = exports.MAIN = void 0;
+let _MAIN = __filename;
+let _ROOT = __dirname;
+let limit = Error.stackTraceLimit;
 Error.stackTraceLimit = Infinity;
 try {
     throw new Error();
 }
 catch (ex) {
-    var lines = ex.stack.toString().split("\n");
-    for (var i = lines.length - 1; i >= 0; i--) {
-        var line = lines[i].trim();
-        var start = line.indexOf("(");
+    const lines = ex.stack.toString().split("\n");
+    for (let i = lines.length - 1; i >= 0; i--) {
+        const line = lines[i].trim();
+        let start = line.indexOf("(");
         if (start < 0)
             continue;
         start++;
-        var end = line.indexOf(")", start);
+        let end = line.indexOf(")", start);
         if (!end)
             continue;
-        var path = line.substring(start, end).trim().replaceAll("\\", "/");
+        let path = line.substring(start, end).trim().replaceAll("\\", "/");
         end = Math.max(path.lastIndexOf("/node_modules/next/dist/"), path.lastIndexOf("/node_modules/lup-auth/"));
-        var notTrimmed = end < 0;
+        let notTrimmed = end < 0;
         path = notTrimmed ? path : path.substring(0, end + 1);
         if (path.length == 0 || path.startsWith("internal/modules/") || path.startsWith("node:") ||
             path.lastIndexOf("/.next/server/") >= 0)
             continue;
         // remove line and column numbers at end
         if (notTrimmed)
-            for (var j = 0; j < 2; j++) {
+            for (let j = 0; j < 2; j++) {
                 end = path.lastIndexOf(":");
                 if (end < 0)
                     break;
@@ -41,10 +44,10 @@ catch (ex) {
 }
 Error.stackTraceLimit = limit;
 /** Absolute path to the main file  */
-export var MAIN = _MAIN;
+exports.MAIN = _MAIN;
 /** Absolte path to root directory in which main file is located (always ends with '/' or '\\') */
-export var ROOT = _ROOT;
-export default {
-    MAIN: MAIN,
-    ROOT: ROOT
+exports.ROOT = _ROOT;
+exports.default = {
+    MAIN: exports.MAIN,
+    ROOT: exports.ROOT
 };
